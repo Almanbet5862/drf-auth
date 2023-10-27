71157985800a20625083d28675a7103e28c57580
Lightking Beknazarov <devlightking@gmail.com>
2023-10-27 22:50:40 +0600

    change urls.py

--- drfauth/urls.py
+++ drfauth/urls.py
@@ -8,8 +8,8 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/v1/doc/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/v1/doc/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/v1/', include('core.urls'), name="api"),
 ]
